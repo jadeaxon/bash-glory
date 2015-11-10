ca5cbe7b1c97165e52fcdab6bccef77de55a9d4e
Jeff Anderson <jeff.anderson@digecor.com>
2015-11-10 14:12:25 -0700

    Add commify()

--- src/string.sh
+++ src/string.sh
@@ -15,12 +15,32 @@ trim() {
 }
 
 
+# Add comma separators to a number and print result.
+commify() {
+    typeset text=${1}
+    typeset bdot=${text%%.*}
+    typeset adot=${text#${bdot}}
+    typeset i commified
+
+    (( i = ${#bdot} - 1 ))
+    while (( i>=3 )) && [[ ${bdot:i-3:1} == [0-9] ]]; do
+        commified=",${bdot:i-2:3}${commified}"
+        (( i -= 3 ))
+    done
+    echo "${bdot:0:i+1}${commified}${adot}"
+}
+
+
+
 #==============================================================================
 # Tests
 #==============================================================================
 
 string__test() {
 	trim '  remove extra whitespace   '
+	echo	
+	commify 1234567890
+	echo
 }
 
 
