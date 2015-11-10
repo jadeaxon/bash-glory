5bbba69f368e6c968abbc79d0ad3572b780f55fb
Jeff Anderson <jeff.anderson@digecor.com>
2015-11-09 21:39:17 -0700

    Add interactive mounting functions to filesystem.sh

--- src/interactive/filesystem.sh
+++ src/interactive/filesystem.sh
@@ -44,6 +44,71 @@ backup() {
 } # backup()
 
 
+#------------------------------------------------------------------------------------------------------------------
+# Mounting
+#------------------------------------------------------------------------------------------------------------------
+
+mount_maxheadroom() {
+	mount -t cifs //maxheadroom.digecor.net/shares -o username=janderson,password=mypassword /mnt/maxheadroom/shares
+}
+
+# Mount C:\ to /mnt/c.  Meant to be called from your VM.
+# Your wireless router should be set to always dole out the same IP address to your laptop.
+# Well, it should be, but it isn't!
+#
+# PRE: Windows: Share your entire C:\ drive as a share named C under Windows.
+#
+# PRE: Ubuntu: apt-get install cifs-utils
+# PRE: See 'Samba.txt'.
+mountc() {
+	if [ "$CYGWIN" ]; then
+		echo "mountc: ERROR: You can't do this from Cygwin!"
+		return 1
+	fi
+
+	# You can add password=<password> to the -o mount options.
+	# The nobrl option makes it so Subversion can work on mounted Windows share.
+	# Without it, you get this kind of error:
+	# svn: E200033: database is locked, executing statement 'RELEASE   s1'
+	local windows windows_user mount
+	mount=/mnt/c
+
+	# NOTE: You have to use an IP address.  Does not translate hostname using /etc/hosts.
+	network=$(ipa | head -1 | cut -d'.' -f1)
+	if [ "$HOSTNAME" == "XPS15-vm" ]; then 
+		windows=192.168.0.19 # TO DO: Make router always assign this by MAC.
+		windows_user=jadeaxon
+		# For XPS 15 Windows 10 Pro laptop at home.
+		mount -o nobrl,user=$windows_user -t cifs //$windows/C $mount
+		(($? == 0)) && cd $mount/Users/$windows_user
+
+	elif ((network == 172)); then
+		# For Windows 10 desktop at digEcor.
+		windows=172.16.40.77
+		windows_user=janderson
+		mount -o nobrl,user=$windows_user -t cifs //$windows/C $mount
+		(($? == 0)) && cd $mount/Users/$windows_user
+
+	else # Unexpected network.
+		echo "mountc: Unexpected network: $network.  Not sure how to mount C: drive."
+		return 1
+	fi
+
+} # mountc()
+
+
+# Lists all mounts nicely formatted if called with no args.  Otherwise, runs normal mount command.
+mount() {
+	if (( $# == 0 )); then
+		# List all mounts in nice columns.
+		command mount | column -t
+	else
+		# There are args, so do a normal mount command.
+		command mount "$@"
+	fi
+} # mount()
+
+
 #==============================================================================
 # Tests
 #==============================================================================
