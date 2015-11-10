d1990e2a8a917e4e3bcbb2b0e71a80a72ca49ead
Jeff Anderson <jeff.anderson@digecor.com>
2015-11-09 20:27:32 -0700

    Add unique_path() and unique_classpath()

--- src/env.sh
+++ src/env.sh
@@ -16,11 +16,50 @@ append_to_path() {
 	fi
 }
 
+
 # Prepends given directory to the PATH env var if not already present.
 prepend_to_path() {
 	if [ -d "$1" ] && [[ ":$PATH:" != *":$1:"* ]]; then
 		PATH="$1":"${PATH}"
 	fi
+}
+
+
+# Removes duplicates from the PATH variable, preserving order.
+# So, to alter path do
+# export PATH=/new/directory:"$PATH"
+# unique_path
+unique_path() {
+    regexp='^\s*$'
+    if [[ $PATH =~ $regexp ]]; then
+        return 0
+    fi
+
+    export PATH=$( \
+        echo $PATH | \
+        perl -pe 's/:/\n/g' | \
+        perl -ne 'print if not $seen{$_}; $seen{$_} = 1' | \
+        perl -pe 's/\n/:/g' | perl -pe 'chop' \
+    )
+}
+
+
+# Removes duplicates from the CLASSPATH variable, preserving order.
+# So, to alter path do
+# export CLASSPATH=/new/directory;"$CLASSPATH"
+# unique_classpath
+unique_classpath() {
+    regexp='^\s*$'
+    if [[ $CLASSPATH =~ $regexp ]]; then
+        return 0
+    fi
+
+    export CLASSPATH=$( \
+        echo $CLASSPATH | \
+        perl -pe 's/;/\n/g' | \
+        perl -ne 'print if not $seen{$_}; $seen{$_} = 1' | \
+        perl -pe 's/\n/;/g' | perl -pe 'chop' \
+    )
 }
 
 
