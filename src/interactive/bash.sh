87c4ec530e7d802a875280084434fdbaff18eb00
Jeff Anderson <jeff.anderson@digecor.com>
2016-09-04 15:45:19 -0700

    Fix prompt when not in a Git repo
    Error message and extra space was being displayed.

--- src/interactive/bash.sh
+++ src/interactive/bash.sh
@@ -6,7 +6,7 @@
 
 # Initializes the Bash prompt.
 init_prompt() {
-	GIT_BRANCH=$(git branch | head -1 | cut -f2 -d' ')	
+	GIT_BRANCH=$(git branch 2> /dev/null | head -1 | cut -f2 -d' ')	
 	PS1_SET_TITLE='\[\e]0;\u@\h:\w\a\]'
 	# \[\e]0;$WINDOWTITLE:\w\a\]
 
@@ -33,7 +33,11 @@ init_prompt() {
 	reset='\[\e[0m\]' # Resets terminal to default settings.
 
 	# PS1='\[\e]0;\w\a\]\n\[\e[32m\]\u@\h \[\e[33m\]\w \[\e[34m\]!\! \[\e[35m\]\T \[\e[0m\]\n\$ '
-	PS1="${init}\n${green}\u@\h ${white}${GIT_BRANCH} ${yellow}\w ${blue}!\! ${magenta}\T ${reset}\n\$ "
+	if [ -z "$GIT_BRANCH" ]; then	
+		PS1="${init}\n${green}\u@\h ${yellow}\w ${blue}!\! ${magenta}\T ${reset}\n\$ "
+	else # We are on a Git branch.
+		PS1="${init}\n${green}\u@\h ${white}${GIT_BRANCH} ${yellow}\w ${blue}!\! ${magenta}\T ${reset}\n\$ "
+	fi
 
 	# This would usually set the Cygwin window title to 'Title'.  However, as PS1 is defined, this 
 	# somehow overrides this.
