63df30b256db2d7be122576ecb3ae3da35edd628
Jeff Anderson <jeff.anderson@digecor.com>
2026-04-29 21:20:34 -0600

    Fix the Google search alias G to work

--- src/interactive/www.sh
+++ src/interactive/www.sh
@@ -7,13 +7,14 @@
 # Functions
 #==============================================================================
 
-
 # Opens up Firefox and searches for args combined into query string.
 query_google() {
 	query="$*" # Combine all args into one big string.
 	url="https://www.google.com/search?q=$query"
-	firefox "$url" &
-
+	# firefox "$url" &
+	# explorer.exe "$url" &
+	# cmd.exe /c start "$url"
+	(cd /mnt/c && cmd.exe /c start "" "$url")
 }
 
 
